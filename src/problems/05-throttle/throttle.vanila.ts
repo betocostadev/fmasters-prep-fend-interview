df2b1f53a5c95f36005c38de89f04606af7b0e3c
betocostadev <robertomarx@gmail.com>
2026-05-20 13:12:47 +0100

    solves easy problems - throttled function

--- src/problems/05-throttle/throttle.vanila.ts
+++ src/problems/05-throttle/throttle.vanila.ts
@@ -1,13 +1,24 @@
 // bun test src/problems/05-throttle/test/throttle.test.ts
 
-export function throttle() {
-
+export function throttle<F extends (...args: any[]) => void>(
+  fn: F,
+  delay: number,
+): (...args: Parameters<F>) => void {
+  let lastCall: number = 0
+  return function throttled(this: unknown, ...args) {
+    const now = Date.now()
+    console.log('Now - last call: ', now - lastCall)
+    if (now - lastCall >= delay) {
+      fn.apply(this, args)
+      lastCall = now
+    }
+  }
 }
 // --- Examples ---
 // Uncomment to test your implementation:
 
-// const log = throttle((msg: string) => console.log(msg), 300)
-// log('a')  // fires immediately → "a"
-// log('b')  // ignored (within 300ms)
-// log('c')  // ignored (within 300ms)
-// setTimeout(() => log('d'), 400)  // fires → "d" (300ms passed)
+const log = throttle((msg: string) => console.log(msg), 300)
+log('fire 1') // fires immediately → "a"
+log('fire 2') // ignored (within 300ms)
+log('fire 3') // ignored (within 300ms)
+setTimeout(() => log('fire 4'), 400) // fires → "d" (300ms passed)
