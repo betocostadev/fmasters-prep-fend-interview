e3c8d84d5b2db7ac4e0de4b8950c4127c21a94b0
betocostadev <robertomarx@gmail.com>
2026-06-08 13:40:06 +0100

    solves vanilla star rating component

--- src/problems/22-star-rating/star-rating.vanila-start.tsx
+++ src/problems/22-star-rating/star-rating.vanila-start.tsx
@@ -1,6 +1,5 @@
 import {AbstractComponent, type TComponentConfig} from '@course/utils'
 import css from './star-rating.module.css'
-import styles from './star-rating.module.css'
 import flex from '@course/styles'
 import cx from "@course/cx";
 
@@ -14,21 +13,15 @@ type TStarRatingProps = {
 }
 
 export class StarRating extends AbstractComponent<TStarRatingProps> {
-    private value: number = 0
+    value: number = 0
 
     /**
      * Step 1: Constructor
      * - Call super() with spread config + className: [css.container] and listeners: ['click']
      * - Initialize this.value from config.value
      */
-    constructor(config: TComponentConfig<TStarRatingProps>) {
-    super({
-      ...config,
-      className: [styles.container],
-      listeners: ['click'],
-    })
-    this.value = config.value
-  }
+    constructor(props: TComponentConfig<TStarRatingProps>) {
+    }
 
     /**
      * Step 2: Handle click event
@@ -38,16 +31,6 @@ export class StarRating extends AbstractComponent<TStarRatingProps> {
      * - If valid: update this.value, call this.config.onValueChange, call this.render()
      */
     onClick({target}: MouseEvent): void {
-        if (this.config.readOnly) return
-
-        if (target instanceof HTMLElement && target.dataset.rating) {
-            const value = Number(target.dataset.rating)
-            if (!isNaN(value)) {
-                this.value = value
-                this.config.onValueChange?.(this.value)
-                this.render()
-            }
-        }
     }
 
     /**
@@ -66,29 +49,9 @@ export class StarRating extends AbstractComponent<TStarRatingProps> {
      * - Include a hidden <input type="hidden" value="${this.value}" aria-hidden="true" />
      */
     toHTML(): string {
-        const stars = Array.from({ length: STARS_COUNT}, (_, index) => this.getStar(index + 1))
-
-        return `
-        <input type="number" value="${this.value}" readonly hidden />
-        <div class="${cx(css.container, flex.flexRowCenter)}" 
-                role="radiogroup" 
-                aria-label="Star rating" 
-                aria-readonly="${String(this.config.readOnly ?? false)}">
-                <span>${stars.join('')}
-            </div>`
     }
 
     getStar = (value: number) => {
-        // The ideal would be to use the Radio Button pattern, but it takes some time to style
-        // So we will use buttons with ARIA attributes to mimic the behavior for now
-       return `<button role="radio" 
-            class="${css.star}"
-            aria-selected="${value === this.value}"
-            data-rating="${value}"
-            data-checked="${value <= this.value}" 
-            aria-label="Rating of ${value}">
-            ${STAR}
-            </button>` 
     }
 
     /**
@@ -99,10 +62,5 @@ export class StarRating extends AbstractComponent<TStarRatingProps> {
      * - Set aria-readonly to String(this.config.readOnly ?? false) on this.container
      */
     afterRender(): void {
-        if (!this.container) return
-
-        this.container.setAttribute('role', 'radiogroup')
-        this.container.setAttribute('aria-label', 'Star Rating')
-        this.container.setAttribute('aria-readonly', String(this.config.readOnly ?? false))
     }
 }
